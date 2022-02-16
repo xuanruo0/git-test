dce655bbecdb7652c853699d5101037256fff327
gaojianwei <1253062093@qq.com>
2022-02-16 15:35:12 +0800

    "first commit"

---
+++ src/main/java/com/ke/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.ke.git;
+
+/**
+ * @outhor 郜建伟
+ * @date 2022-02-16- 15:32
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello.git");
+    }
+}
