dc28d7cd1a780396aec5bf7ff70a264967108a2d
gaojianwei <1253062093@qq.com>
2022-02-16 16:30:27 +0800

    "99 commit"

--- src/main/java/com/ke/git/GitTest.java
+++ src/main/java/com/ke/git/GitTest.java
@@ -9,5 +9,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello.git0");
         System.out.println("hello.git1");
+        System.out.println("hello.git1");
+        System.out.println("hello.git1");
     }
 }
