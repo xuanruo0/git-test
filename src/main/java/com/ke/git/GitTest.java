ff4ad7d21a46504b995d3f1320e97743ecc95258
xuanruo0 <99729351+xuanruo0@users.noreply.github.com>
2022-02-16 17:11:14 +0800

    Update GitTest.java

--- src/main/java/com/ke/git/GitTest.java
+++ src/main/java/com/ke/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hello.git1");
         System.out.println("hello.git99");
         System.out.println("push commit");
+        System.out.println("pull commit");
     }
 }
