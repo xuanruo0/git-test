cc2bc8092c760ddfaac464b32f574614bec48b5a
gaojianwei <1253062093@qq.com>
2022-02-16 16:28:30 +0800

    "hot-fix commit"

--- src/main/java/com/ke/git/GitTest.java
+++ src/main/java/com/ke/git/GitTest.java
@@ -9,8 +9,5 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello.git0");
         System.out.println("hello.git1");
-        System.out.println("hello.git2");
-        System.out.println("hello.git3");
-        System.out.println("hot-fix commit");
     }
 }
